b6882276c0c30230d3e2f3aa8f20af87d3847d04
vepsunonline <48080547+vepsunonline@users.noreply.github.com>
2019-06-16 12:55:08 +0530

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void mainj( String[] args )
     {
         System.out.println( "Hello World!" );
     }
